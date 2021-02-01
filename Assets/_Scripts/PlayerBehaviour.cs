a04326743f5c0a348032a098dff78a3e61ac7e17
Meghna <bilimoriameghna12@gmail.com>
2021-02-01 03:06:08 -0500

    Version 02

--- Assets/_Scripts/PlayerBehaviour.cs
+++ Assets/_Scripts/PlayerBehaviour.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class PlayerBehaviour : MonoBehaviour
 {
     public float movementForce;
+    public float jumpForce;
     public Rigidbody rigidBody;
+    public bool isGrounded;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,17 +18,49 @@ public class PlayerBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetAxisRaw("Horizontal") > 0)
+        if (isGrounded)
         {
-            //Debug.Log("Move Right");
-            //rigidBody.AddForce(new Vector3(1.0f, 0.0f, 0.0f)*movementForce);
-            rigidBody.AddForce(Vector3.right * movementForce);
+            if (Input.GetAxisRaw("Horizontal") > 0)
+            {
+                //Debug.Log("Move Right");
+                //rigidBody.AddForce(new Vector3(1.0f, 0.0f, 0.0f)*movementForce);
+                rigidBody.AddForce(Vector3.right * movementForce);
+            }
+            if (Input.GetAxisRaw("Horizontal") < 0)
+            {
+                // Debug.Log("Move Left");
+                //rigidBody.AddForce(new Vector3(-1.0f, 0.0f, 0.0f) * movementForce);
+                rigidBody.AddForce(Vector3.left * movementForce);
+            }
+            if (Input.GetAxisRaw("Vertical") > 0)
+            {
+                //Debug.Log("Move Right");
+                //rigidBody.AddForce(new Vector3(1.0f, 0.0f, 0.0f)*movementForce);
+                rigidBody.AddForce(Vector3.forward * movementForce);
+            }
+            if (Input.GetAxisRaw("Vertical") < 0)
+            {
+                // Debug.Log("Move Left");
+                rigidBody.AddForce(Vector3.back * movementForce);
+            }
+            if (Input.GetAxisRaw("Jump") > 0)
+                rigidBody.AddForce(Vector3.up * jumpForce);
         }
-        if(Input.GetAxisRaw("Horizontal") < 0)
-        {
-            // Debug.Log("Move Left");
-            rigidBody.AddForce(new Vector3(-1.0f, 0.0f, 0.0f) * movementForce);
-            rigidBody.AddForce(Vector3.left * movementForce);
-        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+            isGrounded = true;
+    }
+    void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+            isGrounded = true;
+    }
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+            isGrounded = false;
     }
 }
